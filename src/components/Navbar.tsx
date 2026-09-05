c1405e513a58fcfc44aa50b49b06b26e0565fdbd
Prajna K <201240380+prajna02@users.noreply.github.com>
2026-09-05 09:56:18 +0530

    Fix navbar build error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { navLinks, profile } from '../data/content'
+import { navLinks } from '../data/content'
 
 export function Navbar() {
   const [open, setOpen] = useState(false)
